ead789f2a1b921cea23ac4cd3a90e9b5069a5286
shihaio <shihao.mymail@gmail.com>
2023-03-19 15:57:00 +0800

    complete Homepage UI, fault: doesnt display homepage

--- tinder-clone/src/pages/Home.js
+++ tinder-clone/src/pages/Home.js
@@ -1,5 +1,22 @@
+import Nav from "../components/Nav";
+
 const Home = () => {
-  return <div>Home</div>;
+  const authToken = false;
+
+  const handleClick = () => {
+    console.log("clicked");
+  };
+  return (
+    <div class="overlay">
+      <Nav minimal={false} authToken={authToken} />
+      <div className="home">
+        <h1>Swipe Right</h1>
+        <button className="primary-button" onClick={handleClick}>
+          {authToken ? "Signout" : "Create Account"}
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
