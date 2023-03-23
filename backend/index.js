c34ad538820cf3bfb78a9e7d5fc528ac8dc4993d
tanwt88 <weitian88.wtt@gmail.com>
2023-03-24 04:58:46 +0800

    Update User with match

--- backend/index.js
+++ backend/index.js
@@ -110,4 +110,25 @@ app.get('/user', async (req, res) => {
     }
 })
 
+// Update User with a match
+app.put('/addmatch', async (req, res) => {
+    const client = new MongoClient(uri)
+    const {userId, matchedUserId} = req.body
+
+    try {
+        await client.connect()
+        const database = client.db('app-data')
+        const users = database.collection('users')
+
+        const query = {user_id: userId}
+        const updateDocument = {
+            $push: {matches: {user_id: matchedUserId}}
+        }
+        const user = await users.updateOne(query, updateDocument)
+        res.send(user)
+    } finally {
+        await client.close()
+    }
+})
+
 app.listen(PORT, () => console.log('server running on PORT ' + PORT))
