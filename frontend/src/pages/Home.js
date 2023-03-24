8ae6544a68cbefe13da691caf31c86ebab37654b
shihaio <shihao.mymail@gmail.com>
2023-03-24 23:26:54 +0800

    solve conflict backend and testing

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,30 +1,30 @@
-import Nav from "../components/Nav";
-import AuthModal from "../components/AuthModal";
-import { useState } from "react";
+import Nav from '../components/Nav'
+import AuthModal from '../components/AuthModal'
+import { useState } from 'react'
 
 const Home = () => {
-  const [showModal, setShowModal] = useState(false);
-  const [isSignUp, setIsSignUp] = useState(true);
+  const [showModal, setShowModal] = useState(false)
+  const [isSignUp, setIsSignUp] = useState(true)
 
-  const authToken = false;
+  const authToken = false
 
   const handleClick = () => {
-    console.log("clicked");
-    setShowModal(true);
-    setIsSignUp(true);
-  };
+    console.log('clicked')
+    setShowModal(true)
+    setIsSignUp(true)
+  }
   return (
-    <div className="overlay">
+    <div className='overlay'>
       <Nav
         minimal={false}
         setShowModal={setShowModal}
         showModal={showModal}
         setIsSignUp={setIsSignUp}
       />
-      <div className="home">
-        <h1 className="primary-title">Swipe Right</h1>
-        <button className="primary-button" onClick={handleClick}>
-          {authToken ? "Signout" : "Create Account"}
+      <div className='home'>
+        <h1 className='primary-title'>Swipe Right</h1>
+        <button className='primary-button' onClick={handleClick}>
+          {authToken ? 'Signout' : 'Create Account'}
         </button>
 
         {showModal && (
@@ -36,7 +36,7 @@ const Home = () => {
         )}
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
