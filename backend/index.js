17a2b2de6b3c180f859dbd122e9ddbc26dbc5143
shihaio <shihao.mymail@gmail.com>
2023-03-24 23:45:53 +0800

    deleted node module in backend and updated environment variable file

--- backend/index.js
+++ backend/index.js
@@ -1,99 +1,95 @@
-const PORT = 8000;
-const express = require("express");
-const { MongoClient } = require("mongodb");
-const jwt = require("jsonwebtoken");
-const { v4: uuidv4 } = require("uuid");
-const cors = require("cors");
-const bcrypt = require("bcrypt");
+const PORT = 8000
+const express = require('express')
+const { MongoClient } = require('mongodb')
+const jwt = require('jsonwebtoken')
+const { v4: uuidv4 } = require('uuid')
+const cors = require('cors')
+const bcrypt = require('bcrypt')
 
-const uri =
-  "mongodb+srv://Morten:123@morten.jayggfk.mongodb.net/?retryWrites=true&w=majority";
+const uri = process.env.URI
 
-const app = express();
-app.use(cors());
-app.use(express.json());
+const app = express()
+app.use(cors())
+app.use(express.json())
 
-app.get("/", (req, res) => {
-  res.json("hello to my app");
-});
+app.get('/', (req, res) => {
+  res.json('hello to my app')
+})
 
-app.post("/signup", async (req, res) => {
-  const client = new MongoClient(uri);
-  const { email, password } = req.body;
+app.post('/signup', async (req, res) => {
+  const client = new MongoClient(uri)
+  const { email, password } = req.body
 
-  const generatedUserId = uuidv4();
-  const hashedPassword = await bcrypt.hash(password, 10);
+  const generatedUserId = uuidv4()
+  const hashedPassword = await bcrypt.hash(password, 10)
 
   try {
-    await client.connect();
-    const database = client.db("tinder-project");
-    const users = database.collection("users");
+    await client.connect()
+    const database = client.db('tinder-project')
+    const users = database.collection('users')
 
-    const existingUser = await users.findOne({ email });
+    const existingUser = await users.findOne({ email })
 
     if (existingUser) {
-      return res.status(409).send("User already exists. Please login");
+      return res.status(409).send('User already exists. Please login')
     }
-    const sanitizedEmail = email.toLowerCase();
+    const sanitizedEmail = email.toLowerCase()
 
     const data = {
       user_id: generatedUserId,
       email: sanitizedEmail,
       hashed_password: hashedPassword,
-    };
-    const insertedUser = await users.insertOne(data);
+    }
+    const insertedUser = await users.insertOne(data)
 
     const token = jwt.sign(insertedUser, sanitizedEmail, {
       expiresIn: 60 * 24,
-    });
+    })
     res
       .status(201)
-      .json({ token, userId: generatedUserId, email: sanitizedEmail });
+      .json({ token, userId: generatedUserId, email: sanitizedEmail })
   } catch (err) {
-    console.log(err);
+    console.log(err)
   }
-});
+})
 
-app.post("/login", async (req, res) => {
-  const client = new MongoClient(uri);
-  const { email, password } = req.body;
+app.post('/login', async (req, res) => {
+  const client = new MongoClient(uri)
+  const { email, password } = req.body
 
   try {
-    await client.connect();
-    const database = client.db("tinder-project");
-    const users = database.collection("users");
+    await client.connect()
+    const database = client.db('tinder-project')
+    const users = database.collection('users')
 
-    const user = await users.findOne({ email });
+    const user = await users.findOne({ email })
 
-    const correctPassword = await bcrypt.compare(
-      password,
-      user.hashed_password
-    );
+    const correctPassword = await bcrypt.compare(password, user.hashed_password)
     if (user && correctPassword) {
       const token = jwt.sign(user, email, {
         expiresIn: 60 * 24,
-      });
-      res.status(201).json({ token, userId: user.user_id, email });
+      })
+      res.status(201).json({ token, userId: user.user_id, email })
     }
-    res.status(400).send("Invalid Credentials");
+    res.status(400).send('Invalid Credentials')
   } catch (err) {
-    console.log(err);
+    console.log(err)
   }
-});
+})
 
-app.get("/users", async (req, res) => {
-  const client = new MongoClient(uri);
+app.get('/users', async (req, res) => {
+  const client = new MongoClient(uri)
 
   try {
-    await client.connect();
-    const database = client.db("tinder-project");
-    const users = database.collection("users");
+    await client.connect()
+    const database = client.db('tinder-project')
+    const users = database.collection('users')
 
-    const returnedUsers = await users.find().toArray();
-    res.send(returnedUsers);
+    const returnedUsers = await users.find().toArray()
+    res.send(returnedUsers)
   } finally {
-    await client.close();
+    await client.close()
   }
-});
+})
 
-app.listen(PORT, () => console.log("server running on PORT, " + PORT));
+app.listen(PORT, () => console.log('server running on PORT, ' + PORT))
